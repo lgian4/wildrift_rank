f5474362de2340705182be7d620b55855d2bddfb
Linargian Pratama <linargian@grip-principle.com>
2024-05-31 18:20:12 +0700

    feat: create hero table

--- sqlite_manager.py
+++ sqlite_manager.py
@@ -1,8 +1,9 @@
 import sqlite3
+DB_FILE_NAME = 'wild_rift.db'
 
 
 def save_list_to_sqlite(list):
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect(DB_FILE_NAME)
     cursor = conn.cursor()
 
     for data in list:
@@ -22,7 +23,7 @@ def get_latest_time_stamp():
 
 
 def run_command(command):
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect(DB_FILE_NAME)
     try:
         cursor = conn.cursor()
         cursor.execute(command)
@@ -34,7 +35,7 @@ def run_command(command):
 
 
 def fetch_one(command):
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect(DB_FILE_NAME)
     try:
         cursor = conn.cursor()
         cursor.execute(command)
@@ -47,11 +48,31 @@ def fetch_one(command):
 
 
 def run_migrations():
-    migrations = [
+    migrations_1 = [
         '''CREATE TABLE IF NOT EXISTS tbl_ranking
-
                       (id INTEGER PRIMARY KEY, time_stamp  integer, date , rank_number integer, role text,hero_img_url text, hero_name text, rank_filter text,  win_rate real, pick_rate real, ban_rate real)''',
-        '''ALTER TABLE tbl_ranking ADD COLUMN  created_at TIMESTAMP ''']
-    for command in migrations:
+        '''ALTER TABLE tbl_ranking ADD COLUMN  created_at TIMESTAMP ''',
+        '''CREATE TABLE IF NOT EXISTS tbl_hero
+                      (id INTEGER PRIMARY KEY, english  text, chinese text , chinese_translation text, img_url text)''',]
+    for command in migrations_1:
         run_command(command)
 
+    hero_count = fetch_one('select count(*) from tbl_hero')
+    if (hero_count is None or hero_count[0] == 0):
+
+        try:
+            with open('hero.csv', 'r', encoding='utf-8') as file:
+                lines = file.readlines()
+                champions = lines[1:]
+                for champion in champions:
+                    conn = sqlite3.connect(DB_FILE_NAME)
+                    cursor = conn.cursor()
+                    data = champion.split(', ')
+                    cursor.execute(
+                        "insert into tbl_hero (chinese,chinese_translation,english,img_url) values (?,?,?,?)",
+                        (data[0], data[1], data[2], data[3]))
+                    conn.commit()
+                    conn.close()
+                    run_command('', )
+        except FileNotFoundError:
+            print(f"File not found.")
